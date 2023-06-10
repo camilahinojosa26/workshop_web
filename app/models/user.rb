aa1db24c090c3b64d4765e5048b6fa14ea616b80
webtech <webtech@webtech-debian.webtech>
2023-06-10 14:09:03 -0400

    tweet db creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  has_many :tweets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
+  
 
 end
