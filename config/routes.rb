657b437157d15ac7a8dc31cfc00e5a7101d3e3d5
webtech <webtech@webtech-debian.webtech>
2023-06-10 12:26:51 -0400

    devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "static_pages#home" #esta es la ruta pricipal con devise si o si se necesita esta root que es una ruta base
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
