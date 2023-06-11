99ed720d4e8374f3cd3749dd3c98c4167e1c0020
webtech <webtech@webtech-debian.webtech>
2023-06-10 21:37:33 -0400

    hashtags

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,6 +30,6 @@ class TweetsController < ApplicationController
 
     private
     def tweet_params
-        params.require(:tweet).permit(:body)
+        params.require(:tweet).permit(:body, :hashtags)
     end   
 end
